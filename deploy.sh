51450808f2664bf6c20f00affda7e9eebaf7e4b1
Bom <31030730+khanitnanj@users.noreply.github.com>
2023-01-13 13:59:20 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-find . -type f -exec curl -u $FTP_USER:$FTP_PASS --ftp-create-dirs -T {} ftp://210.116.80.33:22000/home/mingu \;
+curl --ftp-create-dirs -T uploadfilename -u $FTP_USER:$FTP_PASS ftp://210.116.80.33:22000/home/mingu \;
